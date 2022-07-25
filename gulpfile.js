f6d9981ba1e54fdebff7efdbfabd8b9fb0480918
TRETYAKweb <tretyakbro@gmail.com>
2022-07-25 13:44:14 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     `${BASE_PATH}/js/main.js`,
   ])
 
